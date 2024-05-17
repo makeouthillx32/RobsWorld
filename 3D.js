3d08e12a146e24a8697aa9864cd67aacddf4777f
unenter <63877528+makeouthillx32@users.noreply.github.com>
2024-05-17 16:40:20 -0700

    Size and positioning

--- 3D.js
+++ 3D.js
@@ -5,11 +5,13 @@ document.addEventListener('DOMContentLoaded', function() {
 
         const modelViewerContainer = document.createElement('div');
         modelViewerContainer.className = 'model-viewer-container';
+
         const modelViewer = document.createElement('model-viewer');
         modelViewer.src = 'images/unenterlogom.glb'; // Path to the GLB file
         modelViewer.setAttribute('alt', '3D model of an object');
         modelViewer.setAttribute('auto-rotate', '');
         modelViewer.setAttribute('camera-controls', '');
+
         modelViewerContainer.appendChild(modelViewer);
         container.appendChild(modelViewerContainer);
     } else {
